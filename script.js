420249c066aacf773e8a4f0d71c28a14be27b063
Blender28 <tomajczykbm19@mail.vmi.edu>
2017-01-12 01:14:20 -0500

    Save my work

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$('#figure1').draggable();
+	$('#figure2').draggable();
+	$('#figure3').draggable();
+	$('#figure4').draggable();
+	$('#figure5').draggable();
+	$('#figure6').draggable();
+	$('#figure7').draggable();
+	$('#figure8').draggable();
+	$('#figure9').draggable();
+	$('#batter1').draggable();
+	$('#batter2').draggable();
+	$('#batter3').draggable();
+	$('#batter4').draggable();
+	$('#ball').draggable();
+});
